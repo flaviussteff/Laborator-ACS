b99ebea79a00269816adc6614489903c19858505
Flavius Habeanu <designer0studio@gmail.com>
2024-10-07 19:26:42 +0300

    finally merge programul ok

--- divizori.c
+++ divizori.c
@@ -2,11 +2,11 @@
 int main()
 {
 	int x;
-	scanf("%d",x);
+	scanf("%d",&x);
 	for (int i=1;i*i<=x;i++)
 	{
 		if (i*i==x) printf("%d \n",i);
-		else if (x%i==0) printf("%d si %d",i,n/i);
+		else if (x%i==0) printf("%d si %d",i,x/i);
 	}
 	return 0;
 }
