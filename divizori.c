266b19bf1aa50a5a6e5d851c414bd2a837f72dbf
Flavius Habeanu <designer0studio@gmail.com>
2024-10-07 19:21:28 +0300

    am modificat ceva versiunea 1

--- divizori.c
+++ divizori.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <stdio.h>
 int main()
 {
 	int x;
@@ -6,7 +6,7 @@ int main()
 	for (int i=1;i*i<=x;i++)
 	{
 		if (i*i==x) printf("%d \n",i);
-		else if (n%i==0) printf("%d si %d",i,n/i);
+		else if (x%i==0) printf("%d si %d",i,n/i);
 	}
 	return 0;
 }
