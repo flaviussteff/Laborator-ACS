d6256bcb2eb48cc4a6fb4da0157a0e4fdbea6ce3
Flavius Habeanu <designer0studio@gmail.com>
2024-10-07 19:47:57 +0300

    iar l-am modificat

--- divizori.c
+++ divizori.c
@@ -5,8 +5,9 @@ int main()
 	scanf("%d",&x);
 	for (int i=1;i*i<=x;i++)
 	{
+		int h=x/i;
 		if (i*i==x) printf("%d \n",i);
-		else if (x%i==0) printf("%d si %d",i,x/i);
+		else if (x%i==0) printf("%d si %d \n",i,h);
 	}
 	return 0;
 }
