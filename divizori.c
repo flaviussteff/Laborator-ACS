05f2b75a8c669e3917ed7d69ffb42d756ec8de64
Flavius Habeanu <designer0studio@gmail.com>
2024-10-07 19:14:29 +0300

    fisier divizori in C

---
+++ divizori.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+int main()
+{
+	int x;
+	scanf("%d",x);
+	for (int i=1;i*i<=x;i++)
+	{
+		if (i*i==x) printf("%d \n",i);
+		else if (n%i==0) printf("%d si %d",i,n/i);
+	}
+	return 0;
+}
